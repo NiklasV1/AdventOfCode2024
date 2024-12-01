fb07dd7296d3291e9ebd3d8c61a4f33ea4886a7f
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-12-01 20:41:26 +0100

    makefile

--- Day_01/Day_01.c
+++ Day_01/Day_01.c
@@ -1,22 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct
 {
   int length;
-  int * inputs;
+  int ** inputs;
 } InputData;
 
 InputData
 readInput()
 {
   FILE * inputFile = fopen("Input.txt", "r");
-  
+  int ** numbers = malloc(sizeof(int *) * 2);
+  int * numbers1 = malloc(sizeof(int) * 1001);
+  int * numbers2 = malloc(sizeof(int) * 1001);
+
+  char line[256];
+  int counter = 0;
+  InputData result;
+
+  numbers[0] = numbers1;
+  numbers[1] = numbers2;
+
+  if (inputFile != NULL)
+  {
+    while (fgets(line, sizeof(line), inputFile))
+    {
+      
+    }
+  }
+  return result;
 }
 
 int
 main()
 {
-
+  printf("Hello World!");
 }
 
