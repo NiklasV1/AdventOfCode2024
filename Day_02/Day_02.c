06338e40ec1f8548e44cfccfbc18c0523c871d1e
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-12-02 21:37:28 +0100

    Day 02 Part 2

--- Day_02/Day_02.c
+++ Day_02/Day_02.c
@@ -9,6 +9,17 @@ typedef struct
   int ** inputs;
 } InputData;
 
+void
+printArray(int * in)
+{
+  for (int i = 0; i < 20; i++)
+  {
+    printf(" %i", in[i]);
+  }
+  printf("\n");
+}
+
+
 char *
 charSplitv2(char * string, char separator)
 {
@@ -64,8 +75,8 @@ readInput()
     {
       if (line[1] != '\0')
       {
-        int * numbers1 = malloc(sizeof(int) * 40);
-        for (int i = 0; i < 40; i++)
+        int * numbers1 = malloc(sizeof(int) * 20);
+        for (int i = 0; i < 20; i++)
         {
           numbers1[i] = 0;
         }
@@ -92,6 +103,199 @@ readInput()
   return result;
 }
 
+int
+checkLine(int * in)
+{
+    char ascending;
+    if (in[0] < in[1] && (in[0] + 3) >= in[1])
+    {
+      ascending = 1;
+    }
+    else if (in[1] < in[0] && (in[1] + 3) >= in[0])
+    {
+      ascending = 0;
+    }
+    else
+    {
+      return 0;
+    }
+    for (int j = 2; j < 20; j++)
+    {
+      if (in[j] == 0)
+      {
+        return 1;
+      }
+      if (ascending == 1)
+      {
+        if (in[j-1] < in[j] && (in[j-1] + 3) >= in[j])
+        {
+          continue;
+        }
+        else
+        {
+          return 0;
+        }
+      }
+      else if (ascending == 0)
+      {
+        if (in[j] < in[j-1] && (in[j] + 3) >= in[j-1])
+        {
+          continue;
+        }
+        else
+        {
+          return 0;
+        }
+      }
+    }
+  return 0;
+}
+
+
+void
+condenseList(int * in, int index)
+{
+  for (int i = index + 1; i < 20; i++)
+  {
+    in[i-1] = in[i];
+    if (in[i] == 0)
+    {
+      return;
+    }
+  }
+}
+
+int
+checkLine2(int * in)
+{
+    char ascending;
+    if (in[0] < in[1] && (in[0] + 3) >= in[1])
+    {
+      ascending = 1;
+    }
+    else if (in[1] < in[0] && (in[1] + 3) >= in[0])
+    {
+      ascending = 0;
+    }
+    else
+    {
+      int list1[20];
+      int list2[20];
+
+      for (int i = 0; i < 20; i++)
+      {
+        list1[i] = in[i];
+        list2[i] = in[i];
+      }
+      condenseList(list1, 0);
+      condenseList(list2, 1);
+      if (checkLine(list1) == 1 || checkLine(list2) == 1)
+      {
+         return 1;
+      }
+      else
+      {
+         return 0;
+      }
+
+      condenseList(in, 0);
+      return checkLine(in);
+    }
+    for (int j = 2; j < 20; j++)
+    {
+      if (in[j] == 0)
+      {
+        return 1;
+      }
+      if (ascending == 1)
+      {
+        if (in[j-1] < in[j] && (in[j-1] + 3) >= in[j])
+        {
+          continue;
+        }
+        else
+        {
+            int list1[20];
+            int list2[20];
+
+            for (int i = 0; i < 20; i++)
+            {
+              list1[i] = in[i];
+              list2[i] = in[i];
+            }
+            condenseList(list1, j-1);
+            condenseList(list2, j);
+            if (checkLine(list1) == 1 || checkLine(list2) == 1)
+            {
+              return 1;
+            }
+            else
+            {
+              return 0;
+            }
+        }
+      }
+      else if (ascending == 0)
+      {
+        if (in[j] < in[j-1] && (in[j] + 3) >= in[j-1])
+        {
+          continue;
+        }
+        else
+        {
+            int list1[20];
+            int list2[20];
+            int list3[20];
+
+            for (int i = 0; i < 20; i++)
+            {
+              list1[i] = in[i];
+              list2[i] = in[i];
+              list3[i] = in[i];
+            }
+            condenseList(list1, j-1);
+            condenseList(list2, j);
+            condenseList(list3, j+1);
+            if (checkLine(list1) == 1 || checkLine(list2) == 1 || checkLine(list3) == 1)
+            {
+              return 1;
+            }
+            else
+            {
+              return 0;
+            }
+          }
+      }
+    }
+  return 0;
+}
+
+
+int
+checkLine22(int * in)
+{
+  int result = 0;
+  for (int i = 0; i < 20; i++)
+  {
+    if (in[i] != 0)
+    {
+      int list[20];
+
+      for (int j = 0; j < 20; j++)
+      {
+        list[j] = in[j];
+      }
+
+      condenseList(list, i);
+      result += checkLine(list);
+    }
+  }
+  if (result > 0)
+  {
+    return 1;
+  }
+  return 0;
+}
 
 int
 main()
@@ -105,49 +309,7 @@ main()
   for (int i = 0; i < input.length; i++)
   {
     int * in = input.inputs[i];
-    char ascending;
-    if (in[0] < in[1] && (in[0] + 3) >= in[1])
-    {
-      ascending = 1;
-    }
-    else if (in[1] < in[0] && (in[1] + 3) >= in[0])
-    {
-      ascending = 0;
-    }
-    else
-    {
-      continue;
-    }
-    for (int j = 2; j < 20; j++)
-    {
-      if (in[j] == 0)
-      {
-        sum++;
-        break;
-      }
-      if (ascending == 1)
-      {
-        if (in[j-1] < in[j] && (in[j-1] + 3) >= in[j])
-        {
-          continue;
-        }
-        else
-        {
-          break;
-        }
-      }
-      else if (ascending == 0)
-      {
-        if (in[j] < in[j-1] && (in[j] + 3) >= in[j-1])
-        {
-          continue;
-        }
-        else
-        {
-          break;
-        }
-      }
-    }
+    sum += checkLine(in);
   }
 
 
@@ -164,51 +326,8 @@ main()
   for (int i = 0; i < input2.length; i++)
   {
     int * in = input2.inputs[i];
-    char ascending;
-    if (in[0] < in[1] && (in[0] + 3) >= in[1])
-    {
-      ascending = 1;
-    }
-    else if (in[1] < in[0] && (in[1] + 3) >= in[0])
-    {
-      ascending = 0;
-    }
-    else
-    {
-      continue;
-    }
-    for (int j = 2; j < 20; j++)
-    {
-      if (in[j] == 0)
-      {
-        sum2++;
-        break;
-      }
-      if (ascending == 1)
-      {
-        if (in[j-1] < in[j] && (in[j-1] + 3) >= in[j])
-        {
-          continue;
-        }
-        else
-        {
-          break;
-        }
-      }
-      else if (ascending == 0)
-      {
-        if (in[j] < in[j-1] && (in[j] + 3) >= in[j-1])
-        {
-          continue;
-        }
-        else
-        {
-          break;
-        }
-      }
-    }
+    sum2 += checkLine22(in); 
   }
-
 
   clock_t end2 = clock();
   double time_spent2 = (double)(end2 - start2) * 1000 / CLOCKS_PER_SEC;
